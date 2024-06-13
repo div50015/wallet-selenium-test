0055d1dc4a3966757fb530eeb54a1183c8d38ad8
igor <div50015@mail.ru>
2024-06-13 11:58:25 +0300

    Add test_busy_name test

--- tests/test_name.py
+++ tests/test_name.py
@@ -13,7 +13,7 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 wait = WebDriverWait(driver, timeout=5, ignored_exceptions=(WebDriverException,))
 
 
-def test_name():
+def test_bad_name():
     driver.get('https://koshelek.ru/authorization/signup')
     shadow_host = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "remoteComponent")))
     shadow_root = shadow_host.shadow_root
@@ -26,6 +26,25 @@ def test_name():
     assert element_span.text == 'Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы'
 
     driver.close()
+
+
+def test_busy_name():
+    driver.get('https://koshelek.ru/authorization/signup')
+    shadow_host = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "remoteComponent")))
+    shadow_root = shadow_host.shadow_root
+    element_form: webdriver = shadow_root.find_element(By.CSS_SELECTOR, 'form')
+    element_div: webdriver = wait.until(
+        lambda driver: element_form.find_element(By.XPATH, ".//div[@data-wi='user-name']"))
+    element_input: webdriver = wait.until(lambda driver: element_div.find_element(By.CSS_SELECTOR, "input"))
+    element_input.send_keys(f'username')
+    element_form.click()
+    WebDriverWait(element_div, timeout=7, poll_frequency=0.1).until(
+        EC.text_to_be_present_in_element((By.XPATH, ".//span[@class='k-text']"), "уже"))
+    element_span: driver = wait.until(lambda driver: element_div.find_element(By.XPATH, ".//span[@class='k-text']"))
+    assert element_span.text == 'Имя пользователя уже занято'
+
+    driver.close()
+
 
 def until1(driver):
     wait.until(EC.visibility_of_element_located((By.XPATH, ".//div[@data-wi='user-name']")))
